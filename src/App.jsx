e92e52c2175604d2698a1a13897a4f58659ae1ed
Heidy Primo <heidyprimo10@hotmail.com>
2024-04-16 16:40:20 -0500

    initializing state to show signs into the board, what into the useState as parameter to entry signs manually

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react"
+
 const TURNS = {
   x: "x",
   o: "o",
 }
 
-const board = Array(9).fill(null)
+// 1. const board = Array(9).fill(null)
 
 const Square= ({ children, updateBoard, index}) => {
   return(
@@ -14,6 +16,13 @@ const Square= ({ children, updateBoard, index}) => {
 }
 
 function App() {
+  //2.
+  // const board = Array(9).fill(null)
+  //3.
+  // const [board, setBoard] = useState(Array(9).fill(null))
+  // console.log(board)
+  //:) podriamos hacerlo manual para verl en screen:
+  const [board, setBoard] = useState(["x", "x", "x", "0", "0", "0", "x", "0", "x"])
   return (
   <main className="board">
     <h1>Triqui</h1> 
@@ -25,7 +34,9 @@ function App() {
             key={index}
             index={index}
            >
-            {index}
+            {/* :) */}
+            {board[index]}
+            {/* {index} */}
            </Square>
           )
         })
